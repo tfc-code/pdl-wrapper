d844361301ea0baae3e17164ccb9e61b237f8dc4
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2018-11-09 14:17:43 -0800

    Add PDL API key upon request

--- pdlwrapper/pdlwrapper.py
+++ pdlwrapper/pdlwrapper.py
@@ -11,11 +11,11 @@ class Client:
     PDL_URL = 'https://api.peopledatalabs.com/v4/person'
     TABLE_NAME = 'pdl_log'
 
-    def __init__(self, account_id: str, env: str, aws_access_key_id: str, aws_secret_access_key: str,
+    def __init__(self, account_id: str, env: str, pdl_api_key: str, aws_access_key_id: str, aws_secret_access_key: str,
                  aws_region_name: str):
 
-        if env not in ["test", "prod"]:
-            raise ValueError("env must be either test or prod")
+        if env not in ["dev", "test", "prod"]:
+            raise ValueError("env must be either dev, test or prod")
 
         dynamodb_resource = boto3.resource(
             'dynamodb',
@@ -26,8 +26,10 @@ class Client:
         self.dynamodb_table = dynamodb_resource.Table(self.TABLE_NAME)
         self.account_id = account_id
         self.env = env
+        self.pdl_api_key = pdl_api_key
 
     def get_person(self, params):
+        params['api_key'] = self.pdl_api_key
         resp = requests.get(self.PDL_URL, params=params)
 
         log_item = {
